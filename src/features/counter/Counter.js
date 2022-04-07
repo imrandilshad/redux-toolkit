932fec0b0c4632932d0ffe8a39f707af744a516c
imran <imran@cielrecruiter.com>
2022-04-07 13:19:51 +0500

    setup the structure of redux toolkit and get the data through redux toolkit

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -5,13 +5,11 @@ import {
   increment,
   incrementByAmount,
   incrementAsync,
-  incrementIfOdd,
-  selectCount,
 } from './counterSlice';
 import styles from './Counter.module.css';
 
-export function Counter() {
-  const count = useSelector(selectCount);
+export function Counter({ handleUserList }) {
+  const { counter } = useSelector((state) => state);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
@@ -22,44 +20,37 @@ export function Counter() {
       <div className={styles.row}>
         <button
           className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
+          aria-label='Decrement value'
+          onClick={() => dispatch(decrement())}>
           -
         </button>
-        <span className={styles.value}>{count}</span>
+        <span className={styles.value}>{counter?.value}</span>
         <button
           className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
+          aria-label='Increment value'
+          onClick={() => dispatch(increment())}>
           +
         </button>
       </div>
       <div className={styles.row}>
         <input
           className={styles.textbox}
-          aria-label="Set increment amount"
+          aria-label='Set increment amount'
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
+          onClick={() => dispatch(incrementByAmount(incrementValue))}>
           Add Amount
         </button>
         <button
           className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
+          onClick={() => dispatch(incrementAsync(incrementValue))}>
           Add Async
         </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
+        <button className={styles.asyncButton} onClick={handleUserList}>
+          show/hide userList
         </button>
       </div>
     </div>
---
+++ src/features/user/userSlice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  userList: [],
+  isLoading: false,
+};
+export const getUserList = createAsyncThunk('getUserList', async () => {
+  const res = await fetch('https://reqres.in/api/users/');
+  const json = await res.json();
+  return json.data;
+});
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUserList.pending, (state) => {
+        state.isLoading = true;
+        state.userList = [];
+        state.isError = null;
+      })
+      .addCase(getUserList.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.userList = action.payload;
+      })
+      .addCase(getUserList.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = action.error;
+      });
+  },
+});
+
+export const {} = userSlice.actions;
+
+export default userSlice.reducer;
